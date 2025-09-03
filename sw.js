05677ffe766a3fa3ebdf4021544453bc08cf4d4a
Claire Naudin <cndweb37@gmail.com>
2025-09-03 16:34:27 +0200

    fix : view icon install

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "quest-adventure-v1";
+const CACHE_NAME = "quest-adventure-v2";
 const urlsToCache = [
   "./",
   "./index.html",
@@ -10,15 +10,37 @@ const urlsToCache = [
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(
+        urlsToCache.map((url) => new Request(url, { cache: "reload" }))
+      );
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches
-      .match(event.request)
-      .then((response) => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
   );
 });
